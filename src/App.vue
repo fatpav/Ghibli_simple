ddf8085f6fdf0135fb4022d0b5f5f6e187f70c32
fatpav <fat_pav@hotmail.com>
2020-10-24 17:02:08 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Welcome to a bunch of stuff about Ghibli films!</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      films: [],
+      selectedFilm: null
+    }
+  },
+  mounted() {
+    this.fetchFilms()
+  },
+
+  methods: {
+    fetchFilms: function () {
+      const request = fetch('https://ghibliapi.herokuapp.com/films')
+      .then(response => response.json())
+      .then(data => this.data = films)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang='css' scoped>
+.app {
+  width: 40%;
 }
 </style>
